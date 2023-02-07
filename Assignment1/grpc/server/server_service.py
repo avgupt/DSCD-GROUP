2d7efc0d2ce1d9e0c5c215eb622da4ef95478c65
Avishi Gupta <avishig012@gmail.com>
2023-02-07 19:27:52 +0530

    Create Server class

--- Assignment1/grpc/server/server_service.py
+++ Assignment1/grpc/server/server_service.py
@@ -47,3 +47,42 @@ class ClientServerServicer(server_pb2_grpc.ClientServerServicer):
             filtered = self.filterArticles(request.date)
 
         return server_pb2.GetArticlesResponse(article_list=filtered)
+
+class Server:
+
+    def __init__(self, port):
+        self.address = "localhost"
+        self.port = port                        # str
+        self.hosted_articles = []               # Article[]
+        self.subscribers = []                   # Client[]
+        self.clientele = []                     # Client[]
+
+        self.name = "server_" + port
+
+    def start(self):
+        if not self.__isRegistered():
+            print("Could not register server")
+        else:
+            self.__serve()
+
+    def __isRegistered(self)->bool:
+        # TODO(guptashelly): call Register RPC and return response
+        return True
+    
+    def __serve(self):
+        # TODO(avishi): Refactor code // Remove plag
+        # Reference: https://realpython.com/python-microservices-grpc/
+
+        server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
+        server_pb2_grpc.add_ClientServerServicer_to_server(
+            ClientServerServicer(), server
+        )
+        server.add_insecure_port("[::]:" + self.port)
+        server.start()
+        server.wait_for_termination()
+
+if __name__ == "__main__":
+    port = input("Enter port for server: ")
+
+    myServer = Server(port)
+    myServer.start()
