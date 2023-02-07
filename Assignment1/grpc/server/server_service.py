c03c7b3e0acedf65b3336608b8d842e791432225
Avishi Gupta <avishig012@gmail.com>
2023-02-07 20:35:50 +0530

    Modify server service

--- Assignment1/grpc/server/server_service.py
+++ Assignment1/grpc/server/server_service.py
@@ -10,21 +10,21 @@ clientele = []
 
 class ClientServerServicer(server_pb2_grpc.ClientServerServicer):
 
-    def filterArticles(self, time):
+    def filterArticles1(self, time):
         filtered = []
         for article in hosted_articles:
             if article.time == time:
                 filtered.append(article)
         return filtered
     
-    def filterArticles(self, time, author):
+    def filterArticles2(self, time, author):
         filtered = []
         for article in hosted_articles:
             if article.time == time and article.author == author:
                 filtered.append(article)
         return filtered
 
-    def filterArticles(self, time, author, article_type):
+    def filterArticles3(self, time, author, article_type):
         filtered = []
         for article in hosted_articles:
             if article.time == time and article.author == author and article.WhichOneof('config') == article_type:
@@ -34,27 +34,27 @@ class ClientServerServicer(server_pb2_grpc.ClientServerServicer):
     def GetArticles(self, request, context):
         # Assuming a valid request
  
-        if request.client_id not in clientele:
-            context.cancel() 
+        # if request.client_id not in clientele:
+        #     context.cancel() 
 
         filtered = []
 
         if request.date and request.author and request.type:
-            filtered = self.filterArticles(request.date, request.author, request.type)
+            filtered = self.filterArticles3(request.date, request.author, request.type)
         elif request.date and request.author:
-            filtered = self.filterArticles(request.date, request.author)
+            filtered = self.filterArticles2(request.date, request.author)
         else:
-            filtered = self.filterArticles(request.date)
+            filtered = self.filterArticles1(request.date)
 
         return server_pb2.GetArticlesResponse(article_list=filtered)
     
     def PublishArticle(self, request, context):
-        if request.client_id not in clientele:
-            context.cancel()
-            return server_pb2.PublishArticleResponse(server_pb2.PublishArticleResponse.Status.FAILED)
+        # if request.client_id not in clientele:
+        #     context.cancel()
+        #     return server_pb2.PublishArticleResponse(server_pb2.PublishArticleResponse.Status.FAILED)
         
         hosted_articles.append(request.article)
-        return server_pb2.PublishArticleResponse(server_pb2.PublishArticleResponse.Status.SUCCESS)
+        return server_pb2.PublishArticleResponse(status=server_pb2.PublishArticleResponse.Status.SUCCESS)
 
 class Server:
 
