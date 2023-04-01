bd9061068607e9bfdd4d957f6f85ac07a97fec24
s-gup <shelly19108@iiitd.ac.in>
2023-04-01 15:30:58 +0530

    test updated

--- Assignment2/primaryBlocking/test_script.py
+++ Assignment2/primaryBlocking/test_script.py
@@ -1,26 +1,31 @@
 import subprocess
 import time
 import client
+from status_pb2 import Status
 
 
 # Define constants
 
-REPLICA_PORTS = [8080, 8081, 8015]
-
-
+REPLICA_PORTS = [8080, 8081, 8082]
+REPLICA_ADDRESSES = ["localhost:8080", "localhost:8081", "localhost:8082"]
+FILE1_NAME = "file1"
+FILE1_CONTENT = "This is file 1 text"
 
 # Start registry server
 registry_process = subprocess.Popen(['python', 'registry_server_service.py'])
-
+print("registry service pid",registry_process.pid)
 # Wait for registry server to start up
 time.sleep(1)
 
 # Start replica servers
 replica1 = subprocess.Popen(['python', 'server_service.py', str(REPLICA_PORTS[0])])
+print("replica1 pid",replica1.pid)
+time.sleep(1)
 replica2 = subprocess.Popen(['python', 'server_service.py', str(REPLICA_PORTS[1])])
-
-
-# Wait for replica servers to start up
+print("replica2 pid",replica2.pid)
+time.sleep(1)
+replica3 = subprocess.Popen(['python', 'server_service.py', str(REPLICA_PORTS[2])])
+print("replica3 pid",replica3.pid)
 time.sleep(1)
 
 # Start clients
@@ -30,12 +35,25 @@ client2 = client.Client()
 # Wait for clients to start up
 time.sleep(1)
 
-write_response1 = client1.write("hello1", "my name is  \n yoyoyo", "localhost:8080")
-read_response1 = client1.read(write_response1.uuid,"localhost:8081")
+write_response1 = client1.write(FILE1_NAME, FILE1_CONTENT, REPLICA_ADDRESSES[0])
+assert write_response1.status.type == Status.STATUS_TYPE.SUCCESS, "write status should be SUCCESS"
+file1_uuid = write_response1.uuid
+
+read_response1 = client1.read(file1_uuid,REPLICA_ADDRESSES[1])
+assert read_response1.status.type == Status.STATUS_TYPE.SUCCESS, "read status should be SUCCESS"
+assert read_response1.file_name == FILE1_NAME, "read file name not correct"
+assert read_response1.content == FILE1_CONTENT, "read file content not correct"
+
+delete_response1 = client2.delete(file1_uuid,REPLICA_ADDRESSES[2])
+assert delete_response1.type == Status.STATUS_TYPE.SUCCESS, "delete status should be SUCCESS"
+
+read_response2 = client1.read(file1_uuid,REPLICA_ADDRESSES[1])
+assert read_response2.status.type == Status.STATUS_TYPE.FAILURE, "read status should be FAILURE"
 
 # Stop replica servers
 replica1.terminate()
 replica2.terminate()
+replica3.terminate()
 
 # Stop registry server
 registry_process.terminate()
