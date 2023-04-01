6cfdfbadcbd0a199ec7ba36d40dc9e6e300484f0
s-gup <shelly19108@iiitd.ac.in>
2023-04-01 16:24:51 +0530

    read from all replicas

--- Assignment2/primaryBlocking/test_script.py
+++ Assignment2/primaryBlocking/test_script.py
@@ -39,16 +39,32 @@ write_response1 = client1.write(FILE1_NAME, FILE1_CONTENT, REPLICA_ADDRESSES[0])
 assert write_response1.status.type == Status.STATUS_TYPE.SUCCESS, "write status should be SUCCESS"
 file1_uuid = write_response1.uuid
 
-read_response1 = client1.read(file1_uuid,REPLICA_ADDRESSES[1])
-assert read_response1.status.type == Status.STATUS_TYPE.SUCCESS, "read status should be SUCCESS"
-assert read_response1.file_name == FILE1_NAME, "read file name not correct"
-assert read_response1.content == FILE1_CONTENT, "read file content not correct"
+read_response1 = client1.read(file1_uuid,REPLICA_ADDRESSES[0])
+assert read_response1.status.type == Status.STATUS_TYPE.SUCCESS, "read replica 1 status should be SUCCESS"
+assert read_response1.file_name == FILE1_NAME, "read replica 1 file name not correct"
+assert read_response1.content == FILE1_CONTENT, "read replica 1 file content not correct"
+
+read_response2 = client1.read(file1_uuid,REPLICA_ADDRESSES[1])
+assert read_response2.status.type == Status.STATUS_TYPE.SUCCESS, "read replica 2 status should be SUCCESS"
+assert read_response2.file_name == FILE1_NAME, "read replica 2 file name not correct"
+assert read_response2.content == FILE1_CONTENT, "read replica 2 file content not correct"
+
+read_response3 = client1.read(file1_uuid,REPLICA_ADDRESSES[2])
+assert read_response3.status.type == Status.STATUS_TYPE.SUCCESS, "read replica 3 status should be SUCCESS"
+assert read_response3.file_name == FILE1_NAME, "read replica 3 file name not correct"
+assert read_response3.content == FILE1_CONTENT, "read replica 3 file content not correct"
 
 delete_response1 = client2.delete(file1_uuid,REPLICA_ADDRESSES[2])
 assert delete_response1.type == Status.STATUS_TYPE.SUCCESS, "delete status should be SUCCESS"
 
-read_response2 = client1.read(file1_uuid,REPLICA_ADDRESSES[1])
-assert read_response2.status.type == Status.STATUS_TYPE.FAILURE, "read status should be FAILURE"
+read_response4 = client1.read(file1_uuid,REPLICA_ADDRESSES[0])
+assert read_response4.status.type == Status.STATUS_TYPE.FAILURE, "read 4 status should be FAILURE"
+
+read_response5 = client1.read(file1_uuid,REPLICA_ADDRESSES[1])
+assert read_response5.status.type == Status.STATUS_TYPE.FAILURE, "read 5 status should be FAILURE"
+
+read_response6 = client1.read(file1_uuid,REPLICA_ADDRESSES[2])
+assert read_response6.status.type == Status.STATUS_TYPE.FAILURE, "read 6 status should be FAILURE"
 
 # Stop replica servers
 replica1.terminate()
